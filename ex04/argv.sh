cdf4efea395d824342986df5538c1afdda246fe7
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2025-12-16 01:05:36 +0000

    ok: piscine web development.
    Some exercises added.

---
+++ ex04/argv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "No arguments supplied"
+	exit 1
+fi
+
+if [ $1 ]; then
+	echo "$1"
+fi
+
+if [ $2 ]; then
+	echo "$2"
+fi
+
+if [ $3 ]; then
+	echo "$3"
+fi
